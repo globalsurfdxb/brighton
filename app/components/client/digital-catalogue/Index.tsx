c18cb3a14e110504ab6263006e69273e5b4e501a
Hisham <hisham@globalsurf.ae>
2026-08-19 15:06:57 +0530

    feat: digital-catalogue page created

---
+++ app/components/client/digital-catalogue/Index.tsx
@@ -0,0 +1,12 @@
+import { digitalCatalogueCtaData } from "./data";
+import Cta from "../common/Cta";
+
+const Index = () => {
+  return (
+    <>
+      <Cta data={digitalCatalogueCtaData} />
+    </>
+  );
+};
+
+export default Index;
---
+++ app/components/client/digital-catalogue/data.ts
@@ -0,0 +1,9 @@
+export const digitalCatalogueCtaData = {
+  title: "Let's Talk Lighting",
+  description:
+    "Discuss your project, technical requirements, or product selection with our specialists.",
+  button: {
+    text: "Connect With Us",
+    href: "#",
+  },
+};
