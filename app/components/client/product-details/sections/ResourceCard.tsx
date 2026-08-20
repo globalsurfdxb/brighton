fa7d2550361cd7797af2ca804ef5d7f77712599b
Hisham <hisham@globalsurf.ae>
2026-08-20 12:32:06 +0530

    feat: product-details page set for responsiveness

--- app/components/client/product-details/sections/ResourceCard.tsx
+++ app/components/client/product-details/sections/ResourceCard.tsx
@@ -13,7 +13,7 @@ type CardProps = {
   textColor?: string;
   iconSrc: string;
   arrowIcon: string;
-  arrowSize?: number;
+  arrowSize?: string;
 };
 
 function FileTypeIcon({
@@ -24,7 +24,7 @@ function FileTypeIcon({
   fileType: string;
 }) {
   return (
-    <div className="relative w-[76.9px] h-[89.04px] flex-shrink-0">
+    <div className="relative w-10 h-12 md:w-12 md:h-14 xl:w-[76.9px] xl:h-[89.04px] flex-shrink-0">
       <Image src={iconSrc} alt={fileType} fill className="object-contain" />
       <span className="absolute inset-0 flex items-center justify-center text-description text-trim text-white">
         {fileType}
@@ -39,31 +39,46 @@ export function ResourceCard({
   textColor = "text-foreground",
   iconSrc,
   arrowIcon,
-  arrowSize = 18,
+  arrowSize = "",
 }: CardProps) {
   return (
-    <Link
-      href={item.href}
-      className={`flex justify-between border border-secondary p-30 rounded-[10px] ${bgColor}`}
+    <div
+      className={`flex justify-between border border-secondary p-3 sm:p-30 rounded-[10px] ${bgColor}`}
     >
       <div className="flex gap-5">
         <FileTypeIcon iconSrc={iconSrc} fileType={item.fileType} />
 
-        <div className="flex flex-col gap-2.5 mt-[14px]">
-          <span className={`text-subtitle text-primary mr-4 ${textColor}`}>{item.name}</span>
+        <div className="flex flex-col gap-2.5 xl:mt-[14px]">
+          <span className={`text-subtitle text-primary mr-4 ${textColor}`}>
+            {item.name}
+          </span>
 
           <div className="flex items-center flex-wrap gap-x-30 gap-y-3">
-            <span className="text-description text-trim text-description-color">{item.meta}</span>
-            <span className="text-description text-trim text-description-color">{item.action}</span>
+            <span className="text-description text-trim text-description-color">
+              {item.meta}
+            </span>
+            <span className="text-description text-trim text-description-color">
+              {item.action}
+            </span>
           </div>
         </div>
       </div>
 
-      <div
-        className={`flex items-center justify-center w-10 h-10 rounded-[5px] border border-secondary shrink-0 `}
+      <Link
+        href={item.href}
+        className={`flex items-center justify-center w-7 h-7 xl:w-10 xl:h-10 rounded-[5px] border border-secondary shrink-0 cursor-pointer group btn-fill-center`}
+        style={{
+          "--fill-color": "var(--color-primary, #0A0A0A)",
+        } as React.CSSProperties}
       >
-        <Image src={arrowIcon} alt="arrow" width={arrowSize} height={arrowSize} className="object-contain" />
-      </div>
-    </Link>
+        <Image
+          src={arrowIcon}
+          alt="arrow"
+          width={30}
+          height={30}
+          className={`object-contain group-hover:invert transition-all duration-500 ${arrowSize}`}
+        />
+      </Link>
+    </div>
   );
 }
--- app/components/client/product-details/sections/Resources.tsx
+++ app/components/client/product-details/sections/Resources.tsx
@@ -1,6 +1,8 @@
 import { resourceData } from "../data";
 import AnimatedTitle from "../../animations/AnimatedTitle";
 import { ResourceCard } from "./ResourceCard";
+import Reveal from "../../animations/RevealItemsOneByOneAnimation";
+import { moveUpV2 } from "../../animations/motionVariants";
 
 export default function SpecifierResources() {
   return (
@@ -12,16 +14,17 @@ export default function SpecifierResources() {
           className="mb-40 section-title"
         />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 3xl:grid-cols-3 gap-30 3xl:gap-[29px] mb-100">
+        <div className="grid grid-cols-1 md:grid-cols-2 3xl:grid-cols-3 gap-3 md:gap-30 3xl:gap-[29px] mb-100">
           {resourceData.specifierResources.map((resource, index) => (
-            <ResourceCard
-              key={index}
-              item={resource}
-              textColor="text-foreground"
-              iconSrc="/assets/icons/certificates/1.svg"
-              arrowIcon="/assets/icons/certificates/resource_down_arrow.svg"
-              arrowSize={18}
-            />
+            <Reveal variants={moveUpV2} delayRange={index * 0.01} key={index}>
+              <ResourceCard
+                item={resource}
+                textColor="text-foreground"
+                iconSrc="/assets/icons/certificates/1.svg"
+                arrowIcon="/assets/icons/certificates/resource_down_arrow.svg"
+                arrowSize={"h-3 w-3 xl:h-[18px] xl:w-[18px]"}
+              />
+            </Reveal>
           ))}
         </div>
 
@@ -32,15 +35,16 @@ export default function SpecifierResources() {
 
         <div className="grid grid-cols-1 md:grid-cols-2 3xl:grid-cols-3 gap-30 3xl:gap-[29px]">
           {resourceData.certifications.map((certification, index) => (
-            <ResourceCard
-              key={index}
-              item={certification}
-              bgColor="bg-cream-background"
-              textColor="text-foreground"
-              iconSrc="/assets/icons/certificates/2.svg"
-              arrowIcon="/assets/icons/certificates/resource_plus.svg"
-              arrowSize={20}
-            />
+            <Reveal variants={moveUpV2} delayRange={index * 0.01} key={index}>
+              <ResourceCard
+                item={certification}
+                bgColor="bg-cream-background"
+                textColor="text-foreground"
+                iconSrc="/assets/icons/certificates/2.svg"
+                arrowIcon="/assets/icons/certificates/resource_plus.svg"
+                arrowSize={"h-3 w-3 xl:h-5 xl:w-5"}
+              />
+            </Reveal>
           ))}
         </div>
       </div>
